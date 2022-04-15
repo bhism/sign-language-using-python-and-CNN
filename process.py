690818f510bb47bbcdc68d6aa2beba369035b551
BHISM <61289719+bhism@users.noreply.github.com>
2022-04-15 16:40:28 +0530

    Update process.py

--- process.py
+++ process.py
@@ -34,7 +34,7 @@ def preprocessing(img0,IMG_SIZE=100):
 
 DATADIR = "data\\"
 
-ALPHABET = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','callme','fingercross'] #array containing letters to categorize and create path to video
+ALPHABET = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9'] #array containing letters to categorize and create path to video
 
 training_data=[]
 
